408211bc2fb24f8ea4f69090906137983a074d8d
David Shapiro <dshapi3@wgu.edu>
2024-03-18 17:30:43 -0700

    F. MainScreenControllerr.java [Lines 32-36 + 58-68] - Amends existing controller method + Adds PostMapping for Buy Now button functionality

--- src/main/java/com/example/demo/controllers/MainScreenControllerr.java
+++ src/main/java/com/example/demo/controllers/MainScreenControllerr.java
@@ -4,12 +4,16 @@ import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.service.PartService;
 import com.example.demo.service.ProductService;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -22,39 +26,45 @@ import java.util.List;
 
 @Controller
 public class MainScreenControllerr {
-   // private final PartRepository partRepository;
-   // private final ProductRepository productRepository;'
+    private final PartService partService;
+    private final ProductService productService;
 
-    private PartService partService;
-    private ProductService productService;
-
-    private List<Part> theParts;
-    private List<Product> theProducts;
-
- /*   public MainScreenControllerr(PartRepository partRepository, ProductRepository productRepository) {
-        this.partRepository = partRepository;
-        this.productRepository = productRepository;
-    }*/
-
-    public MainScreenControllerr(PartService partService,ProductService productService){
-        this.partService=partService;
-        this.productService=productService;
+    @Autowired
+    public MainScreenControllerr(PartService partService, ProductService productService) {
+        this.partService = partService;
+        this.productService = productService;
     }
+
+
     @GetMapping("/mainscreen")
-    public String listPartsandProducts(Model theModel, @Param("partkeyword") String partkeyword, @Param("productkeyword") String productkeyword){
+    public String listPartsandProducts(Model theModel, @Param("partkeyword") String partkeyword, @Param("productkeyword") String productkeyword) {
         //add to the sprig model
-        List<Part> partList=partService.listAll(partkeyword);
-        theModel.addAttribute("parts",partList);
-        theModel.addAttribute("partkeyword",partkeyword);
-    //    theModel.addAttribute("products",productService.findAll());
-        List<Product> productList=productService.listAll(productkeyword);
+        List<Part> partList = partService.listAll(partkeyword);
+        theModel.addAttribute("parts", partList);
+        theModel.addAttribute("partkeyword", partkeyword);
+        //    theModel.addAttribute("products",productService.findAll());
+        List<Product> productList = productService.listAll(productkeyword);
         theModel.addAttribute("products", productList);
-        theModel.addAttribute("productkeyword",productkeyword);
+        theModel.addAttribute("productkeyword", productkeyword);
         return "mainscreen";
     }
 
     @GetMapping("/About")
     public String About() {
         return "About";
+    }
+
+
+    @PostMapping("/buyProduct")
+    public String buyProduct(@RequestParam("productId") Long productId, Model model) {
+        boolean success = productService.decrementInventory(productId);
+
+        if (success) {
+            model.addAttribute("message", "Product purchased successfully!");
+            return "purchaseSuccessful";
+        } else {
+            model.addAttribute("message", "Product could not be purchased (out of stock or not found)!");
+            return "purchaseError";
+        }
     }
 }

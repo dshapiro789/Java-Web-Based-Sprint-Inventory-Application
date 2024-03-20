d19a0482029f74259fc3fb82ec496d4316b9967d
David Shapiro <dshapi3@wgu.edu>
2024-03-19 19:43:56 -0700

    G. OutsourcedPart.java [Line 18 + 21-25] - Calls constructors of Part

--- src/main/java/com/example/demo/domain/OutsourcedPart.java
+++ src/main/java/com/example/demo/domain/OutsourcedPart.java
@@ -15,6 +15,13 @@ public class OutsourcedPart extends Part{
 String companyName;
 
     public OutsourcedPart() {
+        super(); // Calls the protected no-argument constructor of Part
+    }
+
+    // Calls another constructor of Part
+    public OutsourcedPart(String name, double price, int inv, int minInv, int maxInv, String companyName) {
+        super(name, price, inv, minInv, maxInv);
+        this.companyName = companyName;
     }
 
     public String getCompanyName() {

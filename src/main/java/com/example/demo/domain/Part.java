91cb6774a813c5a1654b791775dddb544f5742e8
David Shapiro <dshapi3@wgu.edu>
2024-03-24 15:54:58 -0700

    J. Part.java [Line 3, 19] - Removes unused "import com.example.demo.validators.ValidDeletePart;" + "@ValidDeletePart" by commenting them out.

--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -1,6 +1,6 @@
 package com.example.demo.domain;
 
-import com.example.demo.validators.ValidDeletePart;
+//import com.example.demo.validators.ValidDeletePart;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
@@ -16,7 +16,7 @@ import java.util.Set;
  *
  */
 @Entity
-@ValidDeletePart
+//@ValidDeletePart
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="part_type",discriminatorType = DiscriminatorType.INTEGER)
 @Table(name="Parts")
